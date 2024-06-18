2ad841fb3e2cb0f4245e712efe127b7d966179d4
DIVYANSHMRAWAL <divyanshmrawal@gmail.com>
2024-06-18 15:04:03 +0530

    Create Positive or nagetive 5.c
    A program to check whether a number is negative or positive.

---
+++ Positive or nagetive 5.c
@@ -0,0 +1,36 @@
+//QUESTION:--
+//Write a program to check whether a given number is Negative or Positive.
+
+//ANSWER:--
+
+#include <stdio.h>
+
+int main() {
+
+  //let user input= x 
+  int x=5;
+
+  //condition...
+  if(x>0){
+  printf ("Number is positive.\n");
+  }
+  else{
+  printf ("Number is negative.\n");
+  }
+
+  //output=Number is positive.
+  
+  //while let user input=y.....
+  int y=-5;
+
+  condition....
+  if (y<0){
+  printf ("Number is negative.");
+  }
+  else{
+  printf ("Number is positive.");
+  }
+  // output=Number is negative.
+
+  return 0;
+}
