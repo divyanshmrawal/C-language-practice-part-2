8498bb7310eead18ff7c5e1cfd4bcf78cac92f6b
DIVYANSHMRAWAL <divyanshmrawal@gmail.com>
2024-06-18 14:35:55 +0530

    Create Introduction of variables 3.c
    Introduction to variables

---
+++ Introduction of variables 3.c
@@ -0,0 +1,22 @@
+//QUESTION:--
+//Give introduction of variables (int , char , float) . 
+
+//ANSWER:--
+#include<stdio.h>
+
+int main (){
+
+/*Use of int variable :-we can store no decimal number */
+
+int x=2;
+
+/*Use of float variable :-we can store a number with decimal */
+
+float y=2.3;
+
+/*Use of char variable:-we can Store character in it*/
+
+Char z="A";
+
+Return 0;
+}
