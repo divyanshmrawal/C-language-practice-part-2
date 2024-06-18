2321427d777f5962c8f88a4491d7b3ed8d441592
DIVYANSHMRAWAL <divyanshmrawal@gmail.com>
2024-06-18 15:23:23 +0530

    Create Introduction to loop 7.c
    Printing numbers form 1 to 10 using loop concept of C language

---
+++ Introduction to loop 7.c
@@ -0,0 +1,20 @@
+//QUESTION:--
+//Write a c program which print numbers from 1 to 10
+
+//ANSWER:--
+#include <stdio.h>
+
+int main() {
+//Creating int variables
+  int i;
+  
+//condition
+  for (i=1;i<11;i++){
+//printing 
+  printf ("%d\n",i);
+  }
+
+  //output-1,2,3,4,5,6,7,8,9,10
+
+ return 0;
+}
