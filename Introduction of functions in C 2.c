6a9e6bc424216a43cc359b6eb6fe872e467c7e2d
DIVYANSHMRAWAL <divyanshmrawal@gmail.com>
2024-06-18 14:28:26 +0530

    Create Introduction of functions in C 2.c
    Introduction of function concept in C language

---
+++ Introduction of functions in C 2.c
@@ -0,0 +1,22 @@
+//QUESTION:--
+//How to use Functions in C language.
+
+//ANSWER:--
+Create an function and print it's value.
+
+//ANSWER:--
+#include <stdio.h>
+
+//Creating function :-
+void myFunction (){
+printf ("Hello world!");
+}
+
+int main() {
+//executing function:-
+  myFunction ();
+
+//Output:Hello world!
+
+  return 0;
+}
